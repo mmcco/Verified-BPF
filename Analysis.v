8a1b2fa137988de09a0ef49b64f4b1c7a99ce67a
Michael McConville <mmcconville ~a~ mykolab ~d~ com>
2015-04-08 20:53:55 -0400

    largely reverted, but new state-change funcs interfere w/ proof

--- Analysis.v
+++ Analysis.v
@@ -32,23 +32,10 @@ Definition get_fin (i:nat) : option (Vectors.Fin.t 16) :=
     | _ => None
   end.
 
-Definition ne_ins := { l : list instr | l <> [] }.
-
-Theorem cons_not_nil : forall A (x:A) (l:list A), (x :: l) <> [].
-Proof.
-  discriminate.
-Qed.
-
-Definition get_ne_ins (l:list instr) : option ne_ins :=
-  match l with
-      | [] => None
-      | a :: rest => Some (exist _ (a :: rest) (cons_not_nil _ a rest))
-    end.
-
 Record vm_state : Type := make_state {
   acc : option imm;
   x_reg : option imm;
-  ins : { l : list instr | l <> [] };
+  ins : list instr;
   pkt : list (Word.word 32);
   smem : scratch_mem
 }.
@@ -61,60 +48,33 @@ Inductive state : Type :=
   | ContState : vm_state -> state
   | End : end_state -> state.
 
-Definition init_state (ins:ne_ins) : state :=
+Definition init_state (ins:list instr) : state :=
   ContState (make_state None None ins [] empty_mem).
 
-Definition change_acc (s:vm_state) (ins:list instr) (new_acc:imm) : state :=
-  match get_ne_ins ins with
-    | None => End (Error "altering acc as last instr")
-    | Some ne_l => 
-        ContState (make_state (Some new_acc) (x_reg s) ne_l (pkt s) (smem s))
-  end.
+Definition change_acc (s:vm_state) (new_acc:imm) : state :=
+  ContState (make_state (Some new_acc) (x_reg s) (ins s) (pkt s) (smem s)).
 
-Definition change_x_reg (s:vm_state) (ins:list instr) (new_x:imm) : state :=
-  match get_ne_ins ins with
-    | None => End (Error "altering acc as last instr")
-    | Some ne_l =>
-        ContState (make_state (acc s) (Some new_x) ne_l (pkt s) (smem s))
-  end.
+Definition change_x_reg (s:vm_state) (new_x:imm) : state :=
+  ContState (make_state (acc s) (Some new_x) (ins s) (pkt s) (smem s)).
 
-Definition change_smem (s:vm_state) (ins:ne_ins) (i:nat) (v:Word.word 32) : option vm_state :=
-  match get_fin i with
-    | Some fin =>
-        let new_mem := Vector.replace (smem s) fin (Some v) in
-        Some (make_state (acc s) (x_reg s) ins (pkt s) new_mem)
-    | None => None
-  end.
-
-Definition skip_n_ni (n:nat) (l:ne_ins) : option ne_ins :=
-    match skipn n (` l) with
-      | [] => None
-      | a :: rest => Some (exist _ (a :: rest) (cons_not_nil _ a rest))
+Definition change_smem (s:vm_state) (i:nat) (v:Word.word 32) : state :=
+    match get_fin i with
+        | Some fin =>
+            let new_mem := Vector.replace (smem s) fin (Some v) in
+            ContState (make_state (acc s) (x_reg s) (ins s) (pkt s) new_mem)
+        | None =>
+            End (Error "accessed uninitialized memory")
     end.
 
 Definition jump (s:vm_state) (n:word 32) : state :=
-  match skip_n_ni (wordToNat n) (ins s) with
-    | Some ins' =>
-        ContState (make_state (acc s) (x_reg s) ins' (pkt s) (smem s))
-    | None =>
-        End (Error "jumped out of bounds")
-  end.
-
-Definition ne_hd : forall (l:list instr), l <> [] -> instr.
-  refine (fun l p =>
-  match l return ((l <> []) -> instr) with
-    | [] => fun H => _
-    | (a :: rest) => fun H => a
-  end p).
-Proof.
-  intuition.
-Qed.
+  ContState (make_state (acc s) (x_reg s) (skipn (wordToNat n) (ins s)) (pkt s) (smem s)).
 
 Definition step (s:vm_state) : state :=
-  let i := ne_hd (` (ins s)) (proj2_sig (ins s)) in
-  let rest := tl (` (ins s)) in
-  match i with
-    | SoloInstr s_op =>
+  match (ins s) with
+    | [] => End (Error "jumped or stepped past last instruction")
+    | (i :: rest) =>
+    match i with
+      | SoloInstr s_op =>
         match s_op with
           | RetA =>
               match acc s with
@@ -124,21 +84,21 @@ Definition step (s:vm_state) : state :=
           | XStoreA =>
               match acc s with
                 | Some acc' =>
-                    change_x_reg s rest acc'
+                    change_x_reg s acc'
                 | None =>
                     End (Error "storing acc to uninitialized x reg")
               end
           | AStoreX =>
               match x_reg s with
                 | Some x' =>
-                    change_acc s rest x'
+                    change_acc s x'
                 | None =>
                     End (Error "storing x reg to uninitialized acc")
               end
               | AddX =>
                         match acc s, x_reg s with
                             | Some acc', Some x' =>
-                                change_acc s rest ((acc') ^+ x')
+                                change_acc s ((acc') ^+ x')
                             | None, _ =>
                                 End (Error "Adding to uninitialized acc")
                             | _, None =>
@@ -147,7 +107,7 @@ Definition step (s:vm_state) : state :=
                     | SubX =>
                         match acc s, x_reg s with
                             | Some acc', Some x' =>
-                                change_acc s rest ((acc') ^- x')
+                                change_acc s ((acc') ^- x')
                             | None, _ =>
                                 End (Error "Subtracting to uninitialized acc")
                             | _, None =>
@@ -156,7 +116,7 @@ Definition step (s:vm_state) : state :=
                     | MulX =>
                         match acc s, x_reg s with
                             | Some acc', Some x' =>
-                                change_acc s rest ((acc') ^* x')
+                                change_acc s ((acc') ^* x')
                             | None, _ =>
                                 End (Error "Multiplying to uninitialized acc")
                             | _, None =>
@@ -167,7 +127,7 @@ Definition step (s:vm_state) : state :=
                     | AndX =>
                         match acc s, x_reg s with
                             | Some acc', Some x' =>
-                                change_acc s rest ((acc') ^& x')
+                                change_acc s ((acc') ^& x')
                             | None, _ =>
                                 End (Error "And-ing to uninitialized acc")
                             | _, None =>
@@ -176,7 +136,7 @@ Definition step (s:vm_state) : state :=
                     | OrX =>
                         match acc s, x_reg s with
                             | Some acc', Some x' =>
-                                change_acc s rest ((acc') ^| x')
+                                change_acc s ((acc') ^| x')
                             | None, _ =>
                                 End (Error "Or-ing to uninitialized acc")
                             | _, None =>
@@ -190,35 +150,35 @@ Definition step (s:vm_state) : state :=
                         End (Error "*** fill in ***")
                     | LdLen =>
                         let pkt_len := Word.natToWord 32 (length (pkt s)) in
-                        change_acc s rest pkt_len
+                        change_acc s pkt_len
                     | LdXLen =>
                         let pkt_len := Word.natToWord 32 (length (pkt s)) in
-                        change_x_reg s rest pkt_len
+                        change_x_reg s pkt_len
                 end
             | ImmInstr i_op i =>
                 match i_op with
                     | RetK =>
                         End (Ret i)
                     | LdImm =>
-                        change_acc s rest i
+                        change_acc s i
                     | AddImm =>
                         match acc s with
                             | Some acc' =>
-                                change_acc s rest ((acc') ^+ i)
+                                change_acc s ((acc') ^+ i)
                             | None =>
                                 End (Error "Adding to uninitialized acc")
                         end
                     | SubImm =>
                         match acc s with
                             | Some acc' =>
-                                change_acc s rest ((acc') ^- i)
+                                change_acc s ((acc') ^- i)
                             | None =>
                                 End (Error "Subtracting to uninitialized acc")
                         end
                     | MulImm =>
                         match acc s with
                             | Some acc' =>
-                                change_acc s rest ((acc') ^* i)
+                                change_acc s ((acc') ^* i)
                             | None =>
                                 End (Error "Multiplying to uninitialized acc")
                         end
@@ -227,14 +187,14 @@ Definition step (s:vm_state) : state :=
                     | AndImm =>
                         match acc s with
                             | Some acc' =>
-                                change_acc s rest ((acc') ^& i)
+                                change_acc s ((acc') ^& i)
                             | None =>
                                 End (Error "And-ing to uninitialized acc")
                         end
                     | OrImm =>
                         match acc s with
                             | Some acc' =>
-                                change_acc s rest ((acc') ^| i)
+                                change_acc s ((acc') ^| i)
                             | None =>
                                 End (Error "Or-ing to uninitialized acc")
                         end
@@ -245,14 +205,14 @@ Definition step (s:vm_state) : state :=
                     | Neg =>
                         match acc s with
                             | Some acc' =>
-                                change_acc s rest (wneg acc')
+                                change_acc s (wneg acc')
                             | None =>
                                 End (Error "Adding to uninitialized acc")
                         end
                     | JmpImm =>
                         jump s i
                     | LdXImm =>
-                        change_x_reg s rest i
+                        change_x_reg s i
                 end
             | MemInstr m_op m_addr =>
                 match m_op with
@@ -313,47 +273,25 @@ Definition step (s:vm_state) : state :=
                     | JAndImm =>
                         End (Error "*** fill in ***")
                 end
+          end
   end.
 
 Definition size (s:state) : nat :=
   match s with
     | End _ => 0
-    | ContState cs => length (` (ins cs))
+    | ContState cs => length (ins cs)
   end.
-
-Definition t (l:list instr) (p:l <> []) : ne_ins := (exist _ l p).
-Print t.
-
-Lemma impl_subset : forall (l:list instr) (p:l <> []),
-  p -> length l = length (` (exist _ l p)).
-
-Definition is_cs (s:state) : Prop :=
-  match s with
-    | End _ => False
-    | ContState _ => True
-  end.
-
-Lemma cs_gz : forall (s:state), is_cs s -> gt (size s) 0.
-Proof.
-  intros.
-  destruct s.
-  simpl. intuition.
 
 Program Fixpoint prog_eval (s: state) { measure (size s) } : end_state :=
   match s with
     | End (e_s) => e_s
     | ContState cs => prog_eval (step cs)
   end.
-(*
-Lemma end_less : forall a b, lt (size (End a)) (size (ContState b)).
-  simpl. intuition.*)
 
 Next Obligation.
   induction (step cs).
-  simpl. 
+  simpl.
   destruct cs. simpl.
-  destruct v. simpl.
-  Case .
 
 (*
    Used to prove that offsets stay on word (and hence instruction)
